dcb8706f39488910dae460fcb21f8753c4c4c3bc
lore64210 <lore64210@gmail.com>
2024-02-25 22:21:21 -0300

    Fix de estilos

--- frontend/src/components/lists/StaticList.jsx
+++ frontend/src/components/lists/StaticList.jsx
@@ -3,7 +3,10 @@ const StaticList = ({ title, guestList }) => {
         <div className="guest-list">
             <h2>{title}</h2>
             {guestList.map((guest) => (
-                <div key={guest.id} className="list-item space-between">
+                <div
+                    key={guest.id}
+                    className="list-item static-list-item space-between"
+                >
                     <p className="list-item-text">
                         {guest.name} {guest.surname}
                     </p>
